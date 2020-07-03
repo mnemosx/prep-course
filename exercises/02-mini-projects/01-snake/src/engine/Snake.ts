dc25514facaa302fd0209d345ab72a41a713f20f
Andis Kačs <mnemosx@gmail.com>
2020-07-03 23:13:08 +0300

    Update Snake.ts

--- exercises/02-mini-projects/01-snake/src/engine/Snake.ts
+++ exercises/02-mini-projects/01-snake/src/engine/Snake.ts
@@ -8,14 +8,19 @@ export class Snake {
   growCells: number = 0
 
   setDirection(direction: Direction) {
+    // priekš testa, kas neļauj iet uz pretējo virzienu
     if(this.direction === "Right" && direction === 'Left') return
     if(this.direction === "Up" && direction === 'Down') return
     if(this.direction === "Left" && direction === 'Right') return
     if(this.direction === "Down" && direction === 'Up') return
+    // sākumā šis
     this.direction = direction
   }
 
   move() {
+//     //sākumā
+//     this.tail.shift()
+//     this.tail.push(this.head)
     const oldHeadPosition = new Cell(this.head.x, this.head.y)
     if(this.direction === 'Right') {
       this.head = new Cell(this.head.x + 1, this.head.y)
@@ -26,7 +31,8 @@ export class Snake {
     } else if (this.direction === 'Left') {
       this.head = new Cell(this.head.x - 1, this.head.y)
     }
-
+    
+    // kad taisa grow testu
     if(this.growCells > 0) {
       this.growCells = this.growCells - 1
     } else {
@@ -44,6 +50,14 @@ export class Snake {
   }
 
   isSnake(cell: Cell): boolean {
+//     var Cell.ts failā ielikt metodi:
+//     isSame(cell: Cell): boolean {
+//       return this.x === cell.x && it.y === cell.y
+//     }
+//     tad sanāk (it) => it.isSame(cell)
+    if(this.tail.find((it) => it.x === cell.x && it.y === cell.y)) {
+       return true
+    }
     return false;
   }
 
