1d548901f9f003f903cfa7e275293799d93d51d5
Andis Kačs <mnemosx@gmail.com>
2020-07-05 21:54:40 +0300

    Update 34-multiply-map.ts

--- exercises/00-warm-up/src/34-multiply-map.ts
+++ exercises/00-warm-up/src/34-multiply-map.ts
@@ -4,7 +4,18 @@ export {};
  * Implement map function which works similarly as https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
  */
 
-const map = () => {};
+const map = (numbers: number[], func: Function) => {
+//   for(let i = 0; i < numbers.length; i++) {
+//     numbers[i] = func(numbers[i])
+//   }
+  
+  // or 
+  let newArr = []
+  numbers.forEach(function(num: number) {
+    newArr.push(func(number))
+  })
+  return newArr
+};
 
 const numbers = [1, 2, 3];
 const doubled = map(numbers, function(number) {
